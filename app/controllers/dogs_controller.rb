9a2df7793ce21f43709fff5ba4e44b7950d786d9
Jamie Reynolds <jamie.s.a.reynolds@gmail.com>
2020-02-05 15:38:41 +0000

    Created Dogs controller

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,34 @@
+class DogsController < ApplicationController
+  # The index logic is for the search form
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[dog_params])
+  end
+
+  def create
+    @dog = Dog.new(params[:dog])
+    @dog.save
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def update
+    @dog = Dog.find(params[dog_params])
+    @dog.update(params[:dog])
+  end
+
+  def edit
+    @dog = Dog.find(params[dog_params])
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:id, :name, :breed, :bio)
+  end
+end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,5 @@ class Dog < ApplicationRecord
   has_many :bookings
   has_many :reviews
   has_many :availabilities
+  validates :name, :breed, :bio, presence: true
 end
