e7f99dd5eea99a3b3dea290423b5f36e1c3b310d
Tom Poulter <tom.poulter.1@outlook.com>
2020-02-06 20:40:18 +0000

    pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'aboutus', to: "aboutus#aboutus"
+  get 'pages/aboutus'
+  root to: 'pages#aboutus', as: 'aboutus'
+  #get 'aboutus', to: "aboutus#aboutus"
   get 'contactus', to: "contactus#contactus"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dogs, only: [:index, :show, :create, :new, :update, :edit] do
