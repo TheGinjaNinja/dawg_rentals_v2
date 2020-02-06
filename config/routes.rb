af1085f5ff9dce63f64dc6f4ca7f287671b341be
Tom Poulter <tom.poulter.1@outlook.com>
2020-02-06 20:13:51 +0000

    contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'aboutus', to: "aboutus#aboutus"
+  get 'contactus', to: "contactus#contactus"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dogs, only: [:index, :show, :create, :new, :update, :edit] do
     resources :availabilities, only: [:index, :show, :create, :new]
