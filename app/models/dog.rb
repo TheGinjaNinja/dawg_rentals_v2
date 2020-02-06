4cbf0a3e027db73e90a9f9b1e9c0d7c8366c1106
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-02-06 21:06:03 +0000

    Added simple form for adding new dog

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,4 +4,26 @@ class Dog < ApplicationRecord
   has_many :reviews
   has_many :availabilities
   validates :name, :breed, :bio, presence: true
+
+  CATEGORIES = ["Husky",
+    "Jack Russel",
+    "Doberman Pincher",
+    "Dachshund (sausage dog)",
+    "Weimaraner",
+    "Welsh Terrier",
+    "Cavachon",
+    "Dandie Dinmont Terrier",
+    "Labradoodle",
+    "Miniature Schnauzer",
+    "Flat-coated Retriever",
+    "Golden Retriever",
+    "German Shepherd",
+    "Border Collie",
+    "Boxer",
+    "Cocker Spaniel",
+    "Springer Spaniel",
+    "Labrador Retriever",
+    "Cockapoo",
+    "Staffordshire Bull Terrier (staffie)"
+  ]
 end
