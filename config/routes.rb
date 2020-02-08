8bd359fc3188eb07822c8874c9dee4470cac3fca
Sophie Archer <hello@sophiearcher.co.uk>
2020-02-08 12:32:21 +0000

    user can view all bookings and delete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dogs, only: [:index, :show, :create, :new, :update, :edit] do
     resources :availabilities, only: [:index, :show, :create, :new]
-    resources :bookings, only: [:show, :create, :new]
+    resources :bookings, only: [:show, :create, :new, :update]
   end
   resources :bookings, only: [:index, :destroy]
   resources :reviews, only: [:index, :show, :create, :new]
