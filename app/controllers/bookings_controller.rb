7a6c4eed060965faef2fe11f08836aadc45d83fa
jonasvanbuel <46282049+jonasvanbuel@users.noreply.github.com>
2020-02-08 16:18:50 +0000

    Revert "Bug fixes"

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,16 +13,14 @@ class BookingsController < ApplicationController
   end
 
   def new
-    @dog = Dog.find(params["dog_id"])
+    @dog = Dog.find(params[:dog_id])
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
-    @dog = Dog.find(params[:dog_id])
-    @booking.dog = Dog.find(params[:dog_id])
-
+    @booking.dog = @dog = Dog.find(params[:dog_id])
     if @booking.save
       redirect_to dog_booking_path(@dog, @booking)
     else
