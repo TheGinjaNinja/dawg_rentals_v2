ef5e0093c2ca8b3d0048057e2b33fc7926315c84
Sophie Archer <hello@sophiearcher.co.uk>
2020-02-13 20:47:34 +0000

    dog DONE

---
+++ app/javascript/packs/text.js
@@ -0,0 +1,13 @@
+import Typed from 'typed.js';
+  const text = document.getElementById('banner-typed-text');
+  console.log(text)
+const loadDynamicBannerText = () => {
+  if (text) {
+    new Typed('#banner-typed-text', {
+      strings: ["Sustainable solutions to take action, now! 🤟"],
+      typeSpeed: 60,
+      loop: true
+    });
+  }
+}
+export { loadDynamicBannerText };
